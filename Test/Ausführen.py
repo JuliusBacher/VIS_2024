dc498d0094e08a11d35776e5759004ab9a4fd8ad
JuliusBacher <JuliusBacher.s2310566023@fhooe.at>
2024-11-26 19:35:31 +0100

    Tutorial bis Paket3

---
+++ Test/Ausführen.py
@@ -0,0 +1,21 @@
+
+
+
+#in Powershell Konsole reinkopiern zum Ausführen
+
+#für main
+C:\Users\bache\AppData\Local\Programs\Python\Python312\python.exe Test\main.py -f Test\all_day.csv
+
+#für main2
+C:\Users\bache\AppData\Local\Programs\Python\Python312\python.exe Test\main2.py -f Test\all_day.csv
+
+#für main3
+C:\Users\bache\AppData\Local\Programs\Python\Python312\python.exe Test\main3.py -f Test\all_day.csv
+
+
+
+
+PS C:\git\VIS2024\VIS_2024> 
+
+
+C:\Users\bache\AppData\Local\Programs\Python\Python312\python.exe Test\main.py -f Test\all_day.csv
