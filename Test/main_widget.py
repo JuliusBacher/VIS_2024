9111d8df0c79e90ccea318440be9d6d6addd5d3c
JuliusBacher <JuliusBacher.s2310566023@fhooe.at>
2024-12-04 15:30:11 +0100

    chapter5funktioniert

--- Test/main_widget.py
+++ Test/main_widget.py
@@ -1,8 +1,11 @@
 
 from __future__ import annotations
 
-from PySide6.QtWidgets import (QHBoxLayout, QHeaderView, QSizePolicy,
-                               QTableView, QWidget)
+from PySide6.QtCore import QDateTime, Qt
+from PySide6.QtGui import QPainter
+from PySide6.QtWidgets import (QWidget, QHeaderView, QHBoxLayout, QTableView,
+                               QSizePolicy)
+from PySide6.QtCharts import QChart, QChartView, QLineSeries, QDateTimeAxis, QValueAxis
 
 from table_model import CustomTableModel
 
@@ -21,13 +24,17 @@ class Widget(QWidget):
         # QTableView Headers
         self.horizontal_header = self.table_view.horizontalHeader()
         self.vertical_header = self.table_view.verticalHeader()
-        self.horizontal_header.setSectionResizeMode(
-                               QHeaderView.ResizeToContents
-                               )
-        self.vertical_header.setSectionResizeMode(
-                             QHeaderView.ResizeToContents
-                             )
+        self.horizontal_header.setSectionResizeMode(QHeaderView.ResizeToContents)
+        self.vertical_header.setSectionResizeMode(QHeaderView.ResizeToContents)
         self.horizontal_header.setStretchLastSection(True)
+
+        # Creating QChart
+        self.chart = QChart()
+        self.chart.setAnimationOptions(QChart.AllAnimations)
+
+        # Creating QChartView
+        self.chart_view = QChartView(self.chart)
+        self.chart_view.setRenderHint(QPainter.Antialiasing)
 
         # QWidget Layout
         self.main_layout = QHBoxLayout()
@@ -38,5 +45,11 @@ class Widget(QWidget):
         self.table_view.setSizePolicy(size)
         self.main_layout.addWidget(self.table_view)
 
+        ## Right Layout
+        size.setHorizontalStretch(4)
+        self.chart_view.setSizePolicy(size)
+        self.main_layout.addWidget(self.chart_view)
+
         # Set the layout to the QWidget
         self.setLayout(self.main_layout)
+        
