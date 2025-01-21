0f02e31e959f8a99d0f5efc4bc0877b12f9cd76e
JuliusBacher <JuliusBacher.s2310566023@fhooe.at>
2025-01-21 17:32:21 +0100

    Zeug ändern

--- Aufgabe_3/mbsModel.py
+++ Aufgabe_3/mbsModel.py
@@ -1,4 +1,3 @@
-
 import inputfilereader
 import body
 import constraint
@@ -11,11 +10,10 @@ import os
 class mbsModel:
     def __init__(self):
         self.__mbsObjectList = []
-    
-    def importFddFile(self,filepath):
-        file_name, file_extension = os.path.splitext(filepath)
 
-        if(file_extension == ".fdd"):
+    def importFddFile(self, filepath):
+        file_name, file_extension = os.path.splitext(filepath)
+        if file_extension == ".fdd":
             self.__mbsObjectList = inputfilereader.readInput(filepath)
         else:
             print("Wrong file type: " + file_extension)
@@ -23,50 +21,51 @@ class mbsModel:
         
         for object in self.__mbsObjectList:
             object.setModelContext(self)
-
         return True
         
-    def exportFdsFile(self,filepath):
-        f = open(filepath,"w")
+    def exportFdsFile(self, filepath):
+        f = open(filepath, "w")
         for object in self.__mbsObjectList:
             object.writeSolverInput(f)
         f.close()
         
-    def loadDatabase(self,database2Load):
+    def loadDatabase(self, database2Load):
         f = open(database2Load)
         data = json.load(f)
         f.close()
         for modelObject in data["modelObjects"]:
-            if(modelObject["type"] == "Body" and modelObject["subtype"] == "Rigid_EulerParameter_PAI"):
+            if modelObject["type"] == "Body" and modelObject["subtype"] == "Rigid_EulerParameter_PAI":
                 self.__mbsObjectList.append(body.rigidBody(parameter=modelObject["parameter"]))
-            elif(modelObject["type"] == "Constraint" and modelObject["subtype"] == "Generic"):
+            elif modelObject["type"] == "Constraint" and modelObject["subtype"] == "Generic":
                 self.__mbsObjectList.append(constraint.genericConstraint(parameter=modelObject["parameter"]))
-            elif(modelObject["type"] == "Force"):
-                if(modelObject["subtype"] == "GenericForce"):
+            elif modelObject["type"] == "Force":
+                if modelObject["subtype"] == "GenericForce":
                     self.__mbsObjectList.append(force.genericForce(parameter=modelObject["parameter"]))
-                elif(modelObject["subtype"] == "GenericTorque"):
+                elif modelObject["subtype"] == "GenericTorque":
                     self.__mbsObjectList.append(force.genericTorque(parameter=modelObject["parameter"]))
-            elif(modelObject["type"] == "Measure"):
+            elif modelObject["type"] == "Measure":
                 self.__mbsObjectList.append(measure.measure(parameter=modelObject["parameter"]))
-            elif(modelObject["type"] == "DataObject" and modelObject["subtype"] == "Parameter"):
+            elif modelObject["type"] == "DataObject" and modelObject["subtype"] == "Parameter":
                 self.__mbsObjectList.append(dataobject.parameter(parameter=modelObject["parameter"]))
 
         return True
 
-    def saveDatabase(self,dataBasePath):
+    def saveDatabase(self, dataBasePath):
         # Serializing json
         modelObjects = []
         for object in self.__mbsObjectList:
-            modelObject = {"type": object.getType(),
-                           "subtype": object.getSubType(),
-                           "parameter": object.parameter}
+            modelObject = {
+                "type": object.getType(),
+                "subtype": object.getSubType(),
+                "parameter": object.parameter
+            }
             modelObjects.append(modelObject)
-        
+
         jDataBase = json.dumps({"modelObjects": modelObjects})
 
         with open(dataBasePath, "w") as outfile:
             outfile.write(jDataBase)
-    
+
     def showModel(self, renderer):
         for object in self.__mbsObjectList:
             object.show(renderer)
